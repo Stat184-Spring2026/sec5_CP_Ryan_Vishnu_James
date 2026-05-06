e4581d347fbada15406ce4d62014e3606f4a0ec9
vzp5252 <vzp5252@psu.edu>
2026-05-06 07:37:03 -0400

    Joining tables R code polished

--- Join Tables NBA Players.R
+++ Join Tables NBA Players.R
@@ -1,6 +1,5 @@
 library(tidyverse)
 
-# Read all player CSV files
 jokic  <- read_csv("jokic.csv")
 lebron <- read_csv("Lebron.csv")
 curry  <- read_csv("Curry.csv")
@@ -15,11 +14,9 @@ durant <- mutate(durant,
 # Combine all players into one dataset
 nba_data <- bind_rows(jokic, lebron, curry, harden, durant)
 
-# Read salaries
 salaries <- read_csv("salaries.csv")
 
 # Create the FINAL table with stats + salaries
 nba_final <- left_join(nba_data, salaries, by = c("player", "season", "age"))
 
-# Save the final combined dataset
 write_csv(nba_final, "nba_data_FINAL.csv")
